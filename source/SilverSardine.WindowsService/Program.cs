b8e4ef6838ba46daacfccfab2651e7b481717740
OE\raviraj.bisram <raviraj.bisram@expresstravelgroup.com.au>
2020-11-18 14:18:13 +1100

    master changes

---
+++ source/SilverSardine.WindowsService/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.ServiceProcess;
+
+namespace SilverSardine.WindowsService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var servicesToRun = new ServiceBase[] 
+            { 
+                new SampleService(),  
+            };
+
+            ServiceBase.Run(servicesToRun);
+        }
+    }
+}
